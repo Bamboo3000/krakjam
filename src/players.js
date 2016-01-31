d5be726f52d1b19f581a90ec7e78a9ca592e0fd4
Bamboo3000 <pabis91@gmail.com>
2016-01-31 02:46:01 +0100

    some

--- src/players.js
+++ src/players.js
@@ -1,81 +1,33 @@
-// var playerFactory = {
-// 	makePlayer: function(_x, _y, _sprite, _input, _key) {
-// 		(_x == null) ? x = 0 : x = _x;
-// 		(_y == null) ? y = 0 : y = _y;
-// 		var sprite = game.add.sprite(x, y, 'player1');
-// 		var player = {
-// 			 sprite: sprite
-// 			,input: null
-// 			,speed: 0
-// 			,speedMod: 0.05
-// 			,isMoving: false
-// 			,process: function(){
+function playersInit()
+{
+      var sprites1 = game.add.group();
+      sprites1.enableBody = true;
+      sprites1.physicsBodyType = Phaser.Physics.ARCADE;
 
-// 			}
-// 		}
-// 		player.input = playerFactory.makeInput(player, _key);
-// 		return player;
-// 	},
-// 	makeInput: function(_player, _key) {
-// 		var input = getKeyCombinations();
-// 		input.up.onDown.add(function(){
-// 			_player.sprite.body.velocity.y = -200;
-// 		});
-// 		input.up.onUp.add(function(){
-// 			_player.sprite.body.velocity.y = 0;
-// 		});
-// 		input.down.onDown.add(function(){
-// 		    _player.sprite.body.velocity.y = 200;
-// 		});
-// 		input.down.onUp.add(function(){
-// 		    _player.sprite.body.velocity.y = 0;
-// 		});
-// 		input.left.onDown.add(function(){
-// 			_player.sprite.body.velocity.x = -200;
-// 		});
-// 		input.left.onUp.add(function(){
-// 			_player.sprite.body.velocity.x = 0;
-// 		});
-// 		input.right.onDown.add(function(){
-// 			_player.sprite.body.velocity.x = 200;
-// 		});
-// 		input.right.onUp.add(function(){
-// 			_player.sprite.body.velocity.x = 0;
-// 		});
-// 		return input;
-// 	},
-// 	getKeyCombinations: function(key) {
-// 		switch(key) {
-// 			case 'w':
-// 				var input = game.input.keyboard.addKeys({
-// 					 'up':		Phaser.KeyCode.W
-// 					,'down':	Phaser.KeyCode.S
-// 					,'left':	Phaser.KeyCode.A
-// 					,'right':	Phaser.KeyCode.D
-// 				});
-// 				break;
-// 			case 'e':
-// 				var input = game.input.keyboard.addKeys({
-// 					 'up':		Phaser.KeyCode.E
-// 					,'down':	Phaser.KeyCode.D
-// 					,'left':	Phaser.KeyCode.S
-// 					,'right':	Phaser.KeyCode.F
-// 				});
-// 				break;
-// 			case 'r':
-// 				var input = game.input.keyboard.addKeys({
-// 					 'up':		Phaser.KeyCode.R
-// 					,'down':	Phaser.KeyCode.F
-// 					,'left':	Phaser.KeyCode.D
-// 					,'right':	Phaser.KeyCode.G
-// 				});
-// 				break;
-// 		}
-// 	}
-// }
+      sprite1 = sprites1.create(50, 80, 'npc');
+      sprite1.animations.add('walk');
+      sprite1.animations.play('walk', 5, true);
+      sprite1.body.collideWorldBounds = true;
+      sprite1.x = 512;
+      sprite1.y = 384;
+      sprite1.anchor.setTo(0.5, 0.5);
+      sprite1.maxHealth = 100;
+      sprite1.health = 100;
 
-var player = {
-	
+      var sprites2 = game.add.group();
+      sprites2.enableBody = true;
+      sprites2.physicsBodyType = Phaser.Physics.ARCADE;
+
+      sprite2 = sprites2.create(50, 80, 'npc2');
+      sprite2.animations.add('walk');
+      sprite2.animations.play('walk', 5, true);
+      sprite2.body.collideWorldBounds = true;
+      sprite2.x = 512;
+      sprite2.y = 384;
+      sprite2.anchor.setTo(0.5, 0.5);
+      sprite2.maxHealth = 100;
+      sprite2.health = 100;
+
+      game.physics.enable(sprite1, Phaser.Physics.ARCADE);
+      game.physics.enable(sprite2, Phaser.Physics.ARCADE);
 }
-
-
